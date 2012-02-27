5b71cdb765d84db2418d3812a006e71d8125f549
Joel Berger <joel.a.berger@gmail.com>
2012-02-27 17:41:43 -0600

    Alien-Base: added new, cflags, and libs methods to A::B. They call data from Alien::MyLib::ConfigData so I'm not sure how to test these abstractly.

--- lib/Alien/Base.pm
+++ lib/Alien/Base.pm
@@ -6,5 +6,38 @@ use warnings;
 our $VERSION = 0.01;
 $VERSION = eval $VERSION;
 
+sub new {
+  my $class = shift;
+
+  my $config = $class . '::ConfigData';
+  eval "require $config";
+  
+  my $self = {
+    config => $config,
+  };
+
+  bless $self, $class;
+
+  return $self;
+}
+
+sub cflags {
+  my $self = shift;
+  my $cflags = $self->config('cflags');
+  return $cflags;
+}
+
+sub libs {
+  my $self = shift;
+  my $libs = $self->config('libs');
+  return $libs;
+}
+
+# helper method to call Alien::MyLib::ConfigData->config(@_)
+sub config {
+  my $self = shift;
+  return $self->{config}->config(@_);
+}
+
 1;
 
