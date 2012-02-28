b97d04780e8bcc22db97f891d2111110adc8c842
Joel Berger <joel.a.berger@gmail.com>
2012-02-28 09:02:00 -0600

    Alien-Base: A::B can now munge relative paths to point to those in the ShareDir. It only does this for flags specified as an arrayref, not a plain string. Is this a good idea?

--- lib/Alien/Base.pm
+++ lib/Alien/Base.pm
@@ -3,6 +3,9 @@ package Alien::Base;
 use strict;
 use warnings;
 
+use File::chdir;
+use File::ShareDir qw/dist_dir/;
+
 our $VERSION = 0.01;
 $VERSION = eval $VERSION;
 
@@ -11,9 +14,15 @@ sub new {
 
   my $config = $class . '::ConfigData';
   eval "require $config";
+
+  my $dist = $class;
+  $dist =~ s/::/-/g;
+
+  my $dist_dir = dist_dir $dist;
   
   my $self = {
     config => $config,
+    share  => $dist_dir,
   };
 
   bless $self, $class;
@@ -24,19 +33,56 @@ sub new {
 sub cflags {
   my $self = shift;
   my $cflags = $self->config('cflags');
-  return $cflags;
+
+  # if $cflags is a string, return it
+  return $cflags unless ref $cflags;
+
+  # otherwise $cflags is an arrayref
+  my @cflags = map { 
+    my $cflags = $_;
+    $cflags =~ s/^-I(.*)/-I$self->rel2abs_share($1)/e;
+    $cflags;
+  } @$cflags;
+
+  return join( ' ', @libs );
 }
 
 sub libs {
   my $self = shift;
   my $libs = $self->config('libs');
-  return $libs;
+
+  # if $libs is a string, return it
+  return $libs unless ref $libs;
+
+  # otherwise $libs is an arrayref
+  my @libs = map { 
+    my $lib = $_;
+    $lib =~ s/^-L(.*)/-L$self->rel2abs_share($1)/e;
+    $lib;
+  } @$libs;
+
+  return join( ' ', @libs );
 }
 
 # helper method to call Alien::MyLib::ConfigData->config(@_)
 sub config {
   my $self = shift;
   return $self->{config}->config(@_);
+}
+
+sub rel2abs_share {
+  my $self  = shift;
+  my ($rel) = @_;
+
+  my $share = $self->{share};
+
+  local $CWD = $share;
+  # special case if relative path is '.'
+  return "$CWD" if $rel eq '.';
+
+  push @CWD, $rel;
+
+  return "$CWD";
 }
 
 1;
