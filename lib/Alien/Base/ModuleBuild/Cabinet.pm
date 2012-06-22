edce9ea92ca8c2a6d108af564eb1841f83871940
Joel Berger <joel.a.berger@gmail.com>
2012-06-22 12:18:00 -0500

    bump version, add changes

--- lib/Alien/Base/ModuleBuild/Cabinet.pm
+++ lib/Alien/Base/ModuleBuild/Cabinet.pm
@@ -3,7 +3,7 @@ package Alien::Base::ModuleBuild::Cabinet;
 use strict;
 use warnings;
 
-our $VERSION = '0.000_019';
+our $VERSION = '0.000_020';
 $VERSION = eval $VERSION;
 
 use Sort::Versions;
--- lib/Alien/Base/ModuleBuild/Repository/FTP.pm
+++ lib/Alien/Base/ModuleBuild/Repository/FTP.pm
@@ -3,7 +3,7 @@ package Alien::Base::ModuleBuild::Repository::FTP;
 use strict;
 use warnings;
 
-our $VERSION = '0.000_019';
+our $VERSION = '0.000_020';
 $VERSION = eval $VERSION;
 
 use parent 'Alien::Base::ModuleBuild::Repository';
