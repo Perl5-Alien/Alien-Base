2283aa08e5f848529d22b39a30dfb9362f5223c2
Joel Berger <joel.a.berger@gmail.com>
2013-12-15 11:14:24 -0600

    tweak comment

--- lib/Alien/Base/ModuleBuild/Repository.pm
+++ lib/Alien/Base/ModuleBuild/Repository.pm
@@ -80,7 +80,7 @@ sub probe {
 # subclasses are expected to provide 
 sub connection { croak "$_[0] doesn't provide 'connection' method" }
 sub list_files { croak "$_[0] doesn't provide 'list_files' method" }
-# must return filename if different from the one passed in
+# get_file must return filename actually used
 sub get_file  { croak "$_[0] doesn't provide 'get_files' method"  }
 
 1;
