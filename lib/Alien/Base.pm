6fda4e6790f6fd4f27d3e6401cb6d2b370e542a1
Graham Ollis <plicease@cpan.org>
2015-01-07 15:12:49 -0500

    version 0.006_02

--- lib/Alien/Base.pm
+++ lib/Alien/Base.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Alien::Base::PkgConfig;
 
-our $VERSION = '0.006_01';
+our $VERSION = '0.006_02';
 $VERSION = eval $VERSION;
 
 use Carp;
--- lib/Alien/Base/PkgConfig.pm
+++ lib/Alien/Base/PkgConfig.pm
@@ -3,7 +3,7 @@ package Alien::Base::PkgConfig;
 use strict;
 use warnings;
 
-our $VERSION = '0.006_01';
+our $VERSION = '0.006_02';
 $VERSION = eval $VERSION;
 
 use Carp;
