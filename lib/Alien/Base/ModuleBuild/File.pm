b331abf861b9100980bbcebf78aadf1caf9ca965
Graham Ollis <perl@wdlabs.com>
2015-02-17 07:15:52 -0500

    version 0.010

--- lib/Alien/Base/ModuleBuild/File.pm
+++ lib/Alien/Base/ModuleBuild/File.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp;
 
-our $VERSION = '0.009_04';
+our $VERSION = '0.010';
 $VERSION = eval $VERSION;
 
 sub new {
--- lib/Alien/Base/PkgConfig.pm
+++ lib/Alien/Base/PkgConfig.pm
@@ -3,7 +3,7 @@ package Alien::Base::PkgConfig;
 use strict;
 use warnings;
 
-our $VERSION = '0.009_04';
+our $VERSION = '0.010';
 $VERSION = eval $VERSION;
 
 use Carp;
