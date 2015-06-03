26c54e38d9f9fb7ac4ca23c40d628ac196779f1d
Graham Ollis <perl@wdlabs.com>
2015-06-03 19:13:08 -0400

    version 0.018_02

--- lib/Alien/Base/ModuleBuild/Utils.pm
+++ lib/Alien/Base/ModuleBuild/Utils.pm
@@ -4,7 +4,7 @@ package Alien::Base::ModuleBuild::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.018_01';
+our $VERSION = '0.018_02';
 $VERSION = eval $VERSION;
 
 use Text::Balanced qw/extract_bracketed extract_delimited extract_multiple/;
--- lib/Alien/Base/PkgConfig.pm
+++ lib/Alien/Base/PkgConfig.pm
@@ -3,7 +3,7 @@ package Alien::Base::PkgConfig;
 use strict;
 use warnings;
 
-our $VERSION = '0.018_01';
+our $VERSION = '0.018_02';
 $VERSION = eval $VERSION;
 
 use Carp;
