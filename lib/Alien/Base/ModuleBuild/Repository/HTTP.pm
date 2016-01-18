7b8bae6f07850ddb9c9eca6dc5c5cf240c0e76ef
Graham Ollis <plicease@cpan.org>
2016-01-18 10:57:11 -0500

    version 0.024_02

--- lib/Alien/Base/ModuleBuild/Repository/HTTP.pm
+++ lib/Alien/Base/ModuleBuild/Repository/HTTP.pm
@@ -3,7 +3,7 @@ package Alien::Base::ModuleBuild::Repository::HTTP;
 use strict;
 use warnings;
 
-our $VERSION = '0.024_01';
+our $VERSION = '0.024_02';
 $VERSION = eval $VERSION;
 
 use Carp;
--- lib/Alien/Base/PkgConfig.pm
+++ lib/Alien/Base/PkgConfig.pm
@@ -3,7 +3,7 @@ package Alien::Base::PkgConfig;
 use strict;
 use warnings;
 
-our $VERSION = '0.024_01';
+our $VERSION = '0.024_02';
 $VERSION = eval $VERSION;
 
 use Carp;
